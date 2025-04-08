7e455f81590c2a1057600a9ff10e55af22a00b68
Sapana Pandey <sapanapandey447@gmail.com>
2025-04-08 14:21:53 +0545

    122

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,8 +1,10 @@
-import {createBlogPost} from '../controllers/blogController.js';
+import {createBlogPost, getALLBlogPosts} from '../controllers/blogController.js';
 import express from "express"; 
 
 const router = express.Router();
 
 router.post('/create', createBlogPost);
+router.get('/get', getALLBlogPosts);
+
 
 export default router;
